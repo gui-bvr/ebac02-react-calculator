625fe9fb4ab15c542e11745c3d4e056aac47458a
Guilherme Viana <guibvianaribeiro@gmail.com>
2023-03-26 15:27:51 -0300

    Alteração da fonte

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+</style>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
